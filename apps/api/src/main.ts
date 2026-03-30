5f8c3a15f6ad436237a2c0f52f0314e54e5634cd
doispsete <c7ppeu@gmail.com>
2026-03-30 14:25:55 -0300

    fix(deploy): revert api prefix and fix prisma migration env-file

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,7 +8,6 @@ import { AllExceptionsFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
 
   // Global Exception Filter
   app.useGlobalFilters(new AllExceptionsFilter());
